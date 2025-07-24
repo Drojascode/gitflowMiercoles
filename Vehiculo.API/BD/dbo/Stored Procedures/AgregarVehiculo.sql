1597124f3e3252021eaaf11a09a218407f12b337
Drojascode <drojas30534@ufide.ac.cr>
2025-07-23 18:49:20 -0600

    Ajuste

--- Vehiculo.API/BD/dbo/Stored Procedures/AgregarVehiculo.sql
+++ Vehiculo.API/BD/dbo/Stored Procedures/AgregarVehiculo.sql
@@ -1,5 +1,5 @@
 ﻿-- =============================================
--- Author:		<Author,,Name>
+-- Author:		Diego
 -- Create date: <Create Date,,>
 -- Description:	<Description,,>
 -- =============================================
